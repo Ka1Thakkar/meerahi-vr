60f09da30de48701830d758196da7efc7b94c7c5
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-04-01 22:53:15 +0530

    style : Backed by section mobile view

--- app/components/Backing.tsx
+++ app/components/Backing.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image';
 
 const Backing = () => {
     return (
-        <div className="py-24 px-40 bg-gray-200/30">
-            <h1 className="text-7xl font-semibold text-center">
+        <div className="py-24 px-10 lg:px-40 bg-gray-200/30">
+            <h1 className="lg:text-7xl text-5xl font-semibold text-center">
                 Empowering you with
             </h1>
-            <div className='flex gap-5 justify-between items-center mt-20'>
+            <div className='flex flex-col lg:flex-row gap-20 lg:justify-between items-center mt-20'>
                 {Backers.map((backer, id) => {
                     return (
-                        <Image src={backer.image} alt={backer.name} quality={100} className='w-4/12' />
+                        <Image src={backer.image} alt={backer.name} quality={100} className='lg:w-4/12 ' />
                     )
                 })}
             </div>
